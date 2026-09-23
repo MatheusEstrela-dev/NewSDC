44b7638cd492b856995a69ab219a51a3f664c1f1
Matheus Estrela <mr.estrela@yahoo.com.br>
2026-09-23 10:22:43 -0300

    fix(demandas): atualiza rotas para usar o novo DemandaController

--- SDC/routes/modules/demandas.php
+++ SDC/routes/modules/demandas.php
@@ -1,53 +1,53 @@
 <?php
 
-use App\Modules\Demandas\Controllers\TaskController;
+use App\Modules\Demandas\Controllers\DemandaController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['web', 'auth'])->group(function () {
 
-    Route::get('/demandas', [TaskController::class, 'index'])
+    Route::get('/demandas', [DemandaController::class, 'index'])
         ->name('demandas.index')
         ->middleware('can:demandas.chamados.view');
 
-    Route::get('/demandas/nova', [TaskController::class, 'create'])
+    Route::get('/demandas/nova', [DemandaController::class, 'create'])
         ->name('demandas.create')
         ->middleware('can:demandas.chamados.create');
 
-    Route::post('/demandas', [TaskController::class, 'store'])
+    Route::post('/demandas', [DemandaController::class, 'store'])
         ->name('demandas.store')
         ->middleware('can:demandas.chamados.create');
 
-    Route::get('/demandas/{id}', [TaskController::class, 'show'])
+    Route::get('/demandas/{id}', [DemandaController::class, 'show'])
         ->name('demandas.show')
         ->middleware('can:demandas.chamados.view');
 
-    Route::post('/demandas/{id}/comentarios', [TaskController::class, 'addComment'])
+    Route::post('/demandas/{id}/comentarios', [DemandaController::class, 'addComment'])
         ->name('demandas.comments.store')
         ->middleware('can:demandas.chamados.view');
 
     Route::prefix('admin/demandas')->name('admin.demandas.')->group(function () {
 
-        Route::get('/export', [TaskController::class, 'export'])
+        Route::get('/export', [DemandaController::class, 'export'])
             ->name('export')
             ->middleware('can:demandas.chamados.export');
 
-        Route::get('/', [TaskController::class, 'adminIndex'])
+        Route::get('/', [DemandaController::class, 'adminIndex'])
             ->name('index')
             ->middleware('can:demandas.chamados.manage');
 
-        Route::post('/{id}/atribuir', [TaskController::class, 'assign'])
+        Route::post('/{id}/atribuir', [DemandaController::class, 'assign'])
             ->name('assign')
             ->middleware('can:demandas.chamados.manage');
 
-        Route::post('/{id}/status', [TaskController::class, 'changeStatus'])
+        Route::post('/{id}/status', [DemandaController::class, 'changeStatus'])
             ->name('change-status')
             ->middleware('can:demandas.chamados.edit');
 
-        Route::put('/{id}', [TaskController::class, 'update'])
+        Route::put('/{id}', [DemandaController::class, 'update'])
             ->name('update')
             ->middleware('can:demandas.chamados.edit');
 
-        Route::delete('/{id}', [TaskController::class, 'destroy'])
+        Route::delete('/{id}', [DemandaController::class, 'destroy'])
             ->name('destroy')
             ->middleware('can:demandas.chamados.delete');
     });
